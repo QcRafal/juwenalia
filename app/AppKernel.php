74550c86a741edc62a8a9697b5eb80e02d161e95
Rafał Kuć <qc.rafal@gmail.com>
2015-09-11 13:00:01 +0200

    OneSky integration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            new Seven\Bundle\OneskyBundle\SevenOneskyBundle(),
+
             new AppBundle\AppBundle(),
         );
 
